dfce9c50e556a8c1e6cb0321a6c807c8aad938c6
donovan powers <donovan powers>
2016-10-23 15:28:10 -0400

    mongoose addition

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,5 +1,19 @@
 exports.init = function(app) {
   app.post('/postPhoto', function(req, res) {
+    processImage(req);
     res.send("image upload succesful");
   });
+}
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ios');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+function processImage(req) {
+  return true;
+
 }
