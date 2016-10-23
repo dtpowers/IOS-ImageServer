53e3f0c09d536fcc19eb2b83c49efa5018abdca3
donovan powers <donovan powers>
2016-10-23 15:35:04 -0400

    seperated mongoose stuff into its own model for l8r

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,17 +1,11 @@
+var db = require("../models/mongooseModel.js")
+
 exports.init = function(app) {
   app.post('/postPhoto', function(req, res) {
     processImage(req);
     res.send("image upload succesful");
   });
 }
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ios');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
 
 function processImage(req) {
   return true;
