5b3acec86db28c52bcd5ddf44781aaea71954b9f
donovan powers <donovan powers>
2016-10-23 16:09:14 -0400

    added TODO and early erd

--- models/mongooseModel.js
+++ models/mongooseModel.js
@@ -5,3 +5,29 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
 });
+
+
+var Schema = mongoose.Schema;
+
+
+var pictureSchema = new Schema({
+  title: String,
+  author: String,
+  body: String,
+  comments: [{
+    body: String,
+    date: Date
+  }],
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs: Number
+  }
+});
+
+
+//temp photo model
