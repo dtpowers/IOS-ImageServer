7289ef291b62f8fdb51aa956b5e2d6501f94fac2
donovan powers <donovan powers>
2016-11-17 22:14:29 -0500

    removed uploader lib anding basic AWS sdk, setting up config

--- models/awsModel.js
+++ models/awsModel.js
@@ -1,67 +1,34 @@
 //Pretty much enviornment vars for AWS authentication
 //Scraped from files in secrets folder
+var bucketName = 'droper';
+var bucketRegion = 'us-west-2';
 var AWSKey = "";
 var AWSSecret = "";
 
 
 var fs = require('fs');
-var Upload = require('s3-uploader');
-exports.get_keys = function() {
-    keyFile = fs.readFile('secrets/accessKey.secret', function(err, data) {
-      AWSKey = data;
-      //console.log("AWS Key is " + data);
-      secretsFile = fs.readFile('secrets/secret.secret', function(err, data) {
-        AWSSecret = data;
-        //console.log("Aws Secret is " + data);
-      });
-    });
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('secrets/config.json');
+
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {
+    Bucket: bucketName
   }
-  //The s3-Uploader library has a dependency that might be problematic for a deploy situation, 
-  //I need to adjust to just used the main S3 SDK or find a way to include imageMagick dpendency
-  /*
-  var client = new Upload('droper', {
-    aws: {
-      path: 'images/',
-      region: 'us-west-2',
-      acl: 'public-read'
-      accessKeyId: AWSKey,
-      secretAccessKey: AWSSecret
-    },
+});
 
-    cleanup: {
-      versions: true,
-      original: true
-    },
-
-    original: {
-      awsImageAcl: 'private'
-    },
-    //only save one slightly compressed version
-    versions: [{
-      maxHeight: 1040,
-      maxWidth: 1040,
-      format: 'jpg',
-      suffix: '-large',
-      quality: 80,
-      awsImageExpires: 31536000,
-      awsImageMaxAge: 31536000
-    }]
-  });
-
-
-  client.upload('pictures/fire.jpg', {}, function(err, versions, meta) {
-    if (err) {
-      throw err;
-    }
-
-    versions.forEach(function(image) {
-      console.log(image.width, image.height, image.url);
-      // 1024 760 https://my-bucket.s3.amazonaws.com/path/110ec58a-a0f2-4ac4-8393-c866d813b8d1.jpg 
+exports.get_keys = function() {
+  keyFile = fs.readFile('secrets/accessKey.secret', function(err, data) {
+    AWSKey = data;
+    //console.log("AWS Key is " + data);
+    secretsFile = fs.readFile('secrets/secret.secret', function(err, data) {
+      AWSSecret = data;
+      //console.log("Aws Secret is " + data);
+      console.log("AWS auth ready to go");
     });
   });
+}
 
-
-  */
 
 var doError = function(e) {
   console.error("ERROR: " + e);
