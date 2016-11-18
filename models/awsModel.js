570d35c4ea363f301ff91f6839c58c1ff5452b62
donovan powers <donovan powers>
2016-11-17 22:25:24 -0500

    test code for AWS file uploads

--- models/awsModel.js
+++ models/awsModel.js
@@ -1,22 +1,57 @@
-//Pretty much enviornment vars for AWS authentication
-//Scraped from files in secrets folder
+//var AWSKey = "";
+//var AWSSecret = "";
+
 var bucketName = 'droper';
 var bucketRegion = 'us-west-2';
-var AWSKey = "";
-var AWSSecret = "";
+var path = require('path');
 
 
 var fs = require('fs');
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('secrets/config.json');
 
-var s3 = new AWS.S3({
-  apiVersion: '2006-03-01',
-  params: {
-    Bucket: bucketName
-  }
-});
+function upload_test(file) {
+  // Create S3 service object
+  s3 = new AWS.S3({
+    apiVersion: '2006-03-01'
+  });
 
+  // call S3 to retrieve upload file to specified bucket
+  var uploadParams = {
+    Bucket: bucketName,
+    Key: '',
+    Body: ''
+  };
+  var fileStream = fs.createReadStream(file);
+  fileStream.on('error', function(err) {
+    console.log('File Error', err);
+  });
+  uploadParams.Body = fileStream;
+  uploadParams.Key = path.basename(file);
+
+  // call S3 to retrieve upload file to specified bucket
+  s3.upload(uploadParams, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    }
+    if (data) {
+      console.log("Upload Success", data.Location);
+    }
+  });
+
+
+
+}
+
+
+
+
+
+
+
+
+
+/*  old method for AUTH, leftover from uploader lib
 exports.get_keys = function() {
   keyFile = fs.readFile('secrets/accessKey.secret', function(err, data) {
     AWSKey = data;
@@ -28,9 +63,4 @@ exports.get_keys = function() {
     });
   });
 }
-
-
-var doError = function(e) {
-  console.error("ERROR: " + e);
-  throw new Error(e);
-}
+*/
